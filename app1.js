7e4ccf9fdc1b98ebc9cc3ccd8de54359f657cf6f
BhavayaGoyal <Bhavaya123>
2025-03-05 01:34:37 +0530

    Initial commit

---
+++ app1.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express();
+const indexRouter=require('./Router/index')
+app.use(express.json())
+app.use('/',indexRouter)
+app.listen(3000, () => {
+    console.log("server started on port 3000")
+    //Hello Everyone 
+    // hey!!!!!!!!!!
+    // bhavaya goyal 123
+});
