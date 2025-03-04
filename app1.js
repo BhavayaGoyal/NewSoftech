43c30833dc5486cd61e4b9b23b65ecb6b9a4befd
BhavayaGoyal <Bhavaya123>
2025-03-05 01:50:02 +0530

    changes

--- app1.js
+++ app1.js
@@ -5,7 +5,7 @@ const indexRouter=require('./Router/index')
 app.use(express.json())
 app.use('/',indexRouter)
 app.listen(3000, () => {
-    console.log("server started on port 3000")
+    //console.log("server started on port 3000")
     //Hello Everyone 
     // hey!!!!!!!!!!
     //happy Birthday
